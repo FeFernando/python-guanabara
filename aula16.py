53c6f9b6f62f87d8a67d6c189c48a41404213e77
Felipe <12felipexd@gmail.com>
2025-08-29 19:44:33 -0300

    Aula 16

---
+++ aula16.py
@@ -0,0 +1,8 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+
+for cont in range(0, len(lanche)):
+    print(f'Vou comer {lanche[cont]}')
+
+#for comida in lanche:
+#   print(f'Vou comer {comida}')
+print('Nossa comi pra caramba!!')
